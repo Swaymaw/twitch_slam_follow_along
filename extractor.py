5dbba490ff21b72b26c7d44bb1e8df0550830ee5
Swaymaw <swaymaw@gmail.com>
2023-11-09 13:21:52 +0530

    added fundamental matrix filter after matching keypoints

--- extractor.py
+++ extractor.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np 
+from skimage.measure import ransac
+from skimage.transform import FundamentalMatrixTransform
 
 class FeatureExtractor(object):
     GX = 16//2 
@@ -23,7 +25,17 @@ class FeatureExtractor(object):
             matches = self.bf.knnMatch(des, self.last['des'], k=2)
             for m, n in matches:
                 if m.distance < 0.75 * n.distance:
-                    ret.append((kps[m.queryIdx], self.last['kps'][m.trainIdx]))
+                    kp1 = kps[m.queryIdx].pt 
+                    kp2 = self.last['kps'][m.trainIdx].pt
+                    ret.append((kp1, kp2))
         
+        #filter
+        if len(ret) > 0:
+            ret = np.array(ret)
+            model, inliers = ransac((ret[:, 0], ret[:, 1]), FundamentalMatrixTransform,
+                                    min_samples=8, residual_threshold=1, max_trials=100)
+
+            ret = ret[inliers]
+        # return 
         self.last = {'kps':kps, 'des':des}
         return ret
